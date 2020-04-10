67b171dc5be32a1e4c0122e8696ee068388ee6cb
Niraj Choubey <nir.choubey.2011@gmail.com>
2020-04-10 11:41:24 +0530

    Create root-bypass-frida.js

---
+++ Insecurebankv2/root-bypass-frida.js
@@ -0,0 +1,22 @@
+Java.perform(function(){
+
+    console.log("\n Root detection bypass with frida");
+    
+    var postlogin = Java.use("com.android.insecurebankv2.PostLogin");
+
+    postlogin.doesSUexist.implementation = function() {
+
+        console("\ninside doesSuExist function");
+        return false;
+    }
+    
+    
+    postlogin.doesSuperuserApkExist.implementation = function(v0){
+
+        console.log("\n varaible passed"+v0);
+
+
+        return false;
+
+            };
+});
