d8211449ef0a760638b2838113a7c3b3a2a2bc81
Niraj Choubey <nir.choubey.2011@gmail.com>
2020-04-10 12:06:37 +0530

    Create root-bypass-frida.py

---
+++ Insecurebankv2/root-bypass-frida.py
@@ -0,0 +1,25 @@
+import frida, time
+
+def on_message(message, data):
+    if message['type'] == 'send':
+        print("[*]{0}".format(message['payload']))
+    else:
+        print(message)
+        print('error')
+
+device = frida.get_usb_device(1)
+
+pid = device.spawn("com.android.insecurebankv2") # you can use attach as well
+
+device.resume(pid)
+
+time.sleep(1) # without it Java.perform silently fails
+session = device.attach(pid)
+
+script = session.create_script(open("root-bypass-frida.js").read())
+
+script.on('message',on_message)
+script.load()
+
+# prevent the python script from terminating
+input()
